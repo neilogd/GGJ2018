ac8a5ca1c58a06f27aa89bbca2e5891515326341
Neil Forbes-Richardson <neilo@neilo.gd>
2018-01-27 00:24:30 +1100

    Tiled2Unity export of the template scene

---
+++ Assets/Tiled2Unity/Meshes/template_tilemap/MeshDepthPatchUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshDepthPatchUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var meshFilter = gameObject.GetComponent<MeshFilter>();
+        var mesh = meshFilter.mesh;
+
+
+
+
+        var layer = GetComponentInParent<Tiled2Unity.TileLayer>();
+        var map = GetComponentInParent<Tiled2Unity.TiledMap>();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
