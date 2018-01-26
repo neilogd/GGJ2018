4ef5b06aed8624788d20cdbf84efe424c916b87f
Dezzles <daniel@dezzles.com>
2018-01-26 22:30:00 +1100

    Tiled2Unity upload

---
+++ Assets/Editor/TiledProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[Tiled2Unity.CustomTiledImporter]
+public class TiledProcessor : Tiled2Unity.ICustomTiledImporter
+{
+
+    public void HandleCustomProperties(GameObject gameObject, IDictionary<string, string> customProperties)
+    {
+        System.Console.WriteLine(customProperties.Keys);
+        if (customProperties.ContainsKey("Terrain"))
+        {
+            // Add the terrain tile game object
+            // StrategyTile tile = gameObject.AddComponent<StrategyTile>();
+            // tile.TileType = customProperties["Terrain"];
+            // tile.TileNote = customProperties["Note"];
+        }
+    }
+
+    public void CustomizePrefab(GameObject prefab)
+    {
+    }
+}
---
+++ Assets/TileMapProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tiled2Unity;
+using UnityEngine;
+
+public class TileMapProcessor : MonoBehaviour {
+
+    public GameObject map;
+
+	// Use this for initialization
+	void Start () {
+        var objects = map.GetComponentsInChildren<TmxObject>();
+        foreach (var obj in objects)
+        {
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
