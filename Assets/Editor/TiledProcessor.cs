1341d9103631d78435d0117bcb2972b1e53cd4a0
Dezzles <daniel@dezzles.com>
2018-01-27 00:11:03 +1100

    Updates to allow for  custom behaviour

--- Assets/Editor/TiledProcessor.cs
+++ Assets/Editor/TiledProcessor.cs
@@ -7,8 +7,14 @@ public class TiledProcessor : Tiled2Unity.ICustomTiledImporter
 
     public void HandleCustomProperties(GameObject gameObject, IDictionary<string, string> customProperties)
     {
-        if (customProperties.ContainsKey("Terrain"))
+        if (customProperties.ContainsKey("Behaviour"))
         {
+            Debug.Log(customProperties.Count);
+            if (customProperties["Behaviour"] == "Spinning")
+            {
+                CustomBehaviour customBehaviour = gameObject.AddComponent<Spinning>();
+                customBehaviour.Import(customProperties);
+            }
             // Add the terrain tile game object
             // StrategyTile tile = gameObject.AddComponent<StrategyTile>();
             // tile.TileType = customProperties["Terrain"];
